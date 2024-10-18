3759d0df7d63ca6d5462c91abd9a6ca1084e8ead
Koushik Das <koushikdas9831@gmail.com>
2024-10-18 19:48:00 +0530

    KarbonCard | Koushikdas | rules.py, model.py, upload.html and result.html added

---
+++ finance_app/model.py
@@ -0,0 +1,18 @@
+# finance_app/model.py
+
+from .rules import latest_financial_index, iscr_flag, total_revenue_5cr_flag, borrowing_to_revenue_flag
+
+def probe_model_5l_profit(data: dict):
+    latest_financial_index_value = latest_financial_index(data)
+
+    total_revenue_5cr_flag_value = total_revenue_5cr_flag(data, latest_financial_index_value)
+    borrowing_to_revenue_flag_value = borrowing_to_revenue_flag(data, latest_financial_index_value)
+    iscr_flag_value = iscr_flag(data, latest_financial_index_value)
+
+    return {
+        "flags": {
+            "TOTAL_REVENUE_5CR_FLAG": total_revenue_5cr_flag_value,
+            "BORROWING_TO_REVENUE_FLAG": borrowing_to_revenue_flag_value,
+            "ISCR_FLAG": iscr_flag_value,
+        }
+    }
--- finance_app/views.py
+++ finance_app/views.py
@@ -1,10 +1,41 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+#
+# # Create your views here.
+# from django.shortcuts import render, redirect
+# from django.http import JsonResponse
+# import json
+# from .utils import probe_model_5l_profit
+#
+# def upload_file(request):
+#     if request.method == "POST":
+#         uploaded_file = request.FILES['data_file']
+#         data = json.load(uploaded_file)
+#         result = probe_model_5l_profit(data["data"])
+#         request.session['result'] = result  # Store result in session
+#         return redirect('result')  # Redirect to result page
+#     return render(request, 'upload.html')
+#
+# def display_result(request):
+#     result = request.session.get('result', None)  # Get result from session
+#     if result is None:
+#         return redirect('upload')  # If no result, go back to upload page
+#     return render(request, 'result.html', {'result': result})
 
-# Create your views here.
+
+
+
+
+
+
+
+
+
+# finance_app/views.py
+
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import json
-from .utils import probe_model_5l_profit
+from .model import probe_model_5l_profit
 
 def upload_file(request):
     if request.method == "POST":
